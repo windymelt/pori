af371dfd927e08922870fed849b773a4d8612ff4
windymelt <windymelt@3qe.us>
2021-02-10 20:58:08 +0900

    removed comments

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use image::GenericImageView;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = &args[1]; // args[0] stands for this command
-    println!("{}", input);
     let mut img: DynamicImage = image::open(input).unwrap();
 
     let count = count_colors(&mut img);
@@ -19,7 +18,6 @@ fn main() {
 
 fn count_colors(img: &mut DynamicImage) -> f32 {
     let mut dict: HashMap<u32, bool> = HashMap::new();
-    //let mut hist: HashMap<u16, u32> = HashMap::new();
     let height = img.height();
     let width = img.width();
     let mut pixel_count: usize = 0;
@@ -34,24 +32,9 @@ fn count_colors(img: &mut DynamicImage) -> f32 {
 
             pixel_count = pixel_count + 1;
             dict.insert(key, true);
-            let idx: u16 = (red as u16 + green as u16 + blue as u16) / 8;
-            //match hist.get(&idx) {
-            //    Some(&cnt) => hist.insert(idx, cnt + 1),
-            //  _ => hist.insert(idx, 1),
-            //};
         }
     }
 
-    // let mut past: u32 = 0;
-    // let mut sumdiff: u64 = 0;
-    // for x in 0..255 {
-    //     // TODO sequential
-    //     sumdiff += ((hist[&x] / pixel_count as u32) as i32 - past as i32)
-    //         .abs()
-    //         .pow(2) as u64;
-    //     past = hist[&x];
-    // }
-    // return sumdiff as f32;
     return dict.len() as f32 / pixel_count as f32;
 }
 
